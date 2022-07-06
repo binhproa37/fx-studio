1de0061e6a1a9cc271406eebd8027ce298b25b72
binhproa37 <63229646+binhproa37@users.noreply.github.com>
2022-07-06 10:43:50 +0700

    Add files via upload

---
+++ NavigationController/NavigationController/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  NavigationController
+//
+//  Created by Bui Dang Nam Binh on 01/07/2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "First VC"
+
+    }
+
+    @IBAction func push(_ sender: Any) {
+        
+    }
+    
+
+}
---
+++ NavigationController/NavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Bui Dang Nam Binh on 01/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
